b817f1ebbfb045d30a7b4e4d9de8e07f4018daa6
kareeemashrafm <kareeemashrafm@gmail.com>
2025-07-24 01:10:48 +0300

    buttons now output numbers and operator functions

---
+++ script.js
@@ -0,0 +1,29 @@
+let value='';
+let sum='';
+
+function setvalue(num){
+  
+value=num;
+console.log(value);
+}
+
+function add(num1,num2){
+
+  sum=num1+num2;
+  console.log(sum);
+}
+
+function subtract(num1,num2){
+
+  sum=num1-num2;
+}
+
+function multiply(num1,num2){
+
+  sum=num1*num2;
+}
+
+function divide(num1,num2){
+
+  sum=num1/num2;
+}
