f8ffc15f4289472119cfe7fe4db5839955a01bf0
kareeemashrafm <kareeemashrafm@gmail.com>
2025-07-25 11:34:36 +0300

    final result

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let value='';
 let sum='';
+let operator='';
+let num1='';
+let num2='';
+let secondnum = false;
+let iscalc=false;
 
 function setvalue(num){
   
@@ -7,23 +12,94 @@ value=num;
 console.log(value);
 }
 
-function add(num1,num2){
-
-  sum=num1+num2;
-  console.log(sum);
-}
-
-function subtract(num1,num2){
-
-  sum=num1-num2;
-}
-
-function multiply(num1,num2){
-
-  sum=num1*num2;
-}
 
 function divide(num1,num2){
-
-  sum=num1/num2;
+  if (num1!=0 && num2!=0 ){
+  sum=num1/num2;}
+  else{alert("Cant divide by 0!")}
 }
+
+function operate(num1,operator,num2){
+
+    num1=parseFloat(num1);
+    num2=parseFloat(num2);
+
+  switch(operator){
+     case '+':
+      return num1 + num2 ;
+    case '-':
+    return num1 - num2
+    case '*':
+      return num1*num2;
+    case '/':
+     if(num1!=0&&num2!=0){return num1/num2;}
+     else alert("You cant divide by 0!")
+  }
+}
+let expression='';
+
+function setdisplay(value) {
+    const screen = document.getElementById('screen');
+
+     if ((value === '+' || value === '-' || value === '*' || value === '/')&&secondnum==false) {
+    if (num1 === '' || operator !== '') return; 
+    operator = value;
+    secondnum = true;
+    screen.value = firstNum + operator;
+  } else {
+
+    if (!secondnum) {
+      num1 += value;
+      screen.value = num1;
+    } else {
+      num2 += value;
+      screen.value = num1 + operator + num2;
+    }
+  }
+
+  if((value === '+'|| value === '-' || value === '*'|| value === '/')&& secondnum===true)
+  {const result = operate(num1,operator,num2);
+  
+  
+  num1=result.toString();
+  num2='';
+  operator=value;
+  secondnum=true;
+  screen.value =result.toFixed(2)+operator;
+  }
+  if((value !== '+'|| value !== '-' || value !== '*'|| value !== '/')&& iscalc===true)
+  {
+    clearscreen();
+    num1=value;
+    screen.value=num1;
+    iscalc=false;
+  }
+
+  }
+
+  function calculate(){
+     const screen = document.getElementById('screen');
+  if (num1 === '' || num2 === '' || operator === '') {
+    screen.value = 'Error';
+    return;
+  }
+
+  const result = operate(num1,operator,num2);
+  screen.value =result.toFixed(2);
+  
+  num1=result.toString();
+  num2='';
+  operator='';
+  secondnum=false;
+  iscalc=true;
+  }
+
+  function clearscreen(){
+
+    num1='';
+    num2='';
+    operator='';
+    secondnum=false;
+    document.getElementById('screen').value='';
+
+  }
